69f90cc579d11b4c13faa5e8a2e60d0c4cea75ff
lisandromoura <lisandro.cabrom@gmail.com>
2024-06-24 09:32:19 -0300

    feat: finish and release version 1.0
    - Your branch is up to date with 'origin/main'.
    -
    - Mudanças a serem submetidas:
    -	modified:   .gitignore
    -	modified:   app/Class/SiteMap.php
    -	deleted:    app/Class/SiteMapImagem.php
    -	modified:   index.php
    -	deleted:    sitemap.xml

--- app/Class/SiteMap.php
+++ app/Class/SiteMap.php
@@ -1,100 +1,140 @@
 <?php
 
 namespace App\Class;
-use mysqli;
+
 use DateTime;
 
 class SiteMap
 {
+    private $lastmod;
     private $priority;
-    public function __construct(String $priority = "1.0") {
-        $this->priority = $priority;
-    }
 
-    public function getPriority() {
-        return $this->priority;
-    }
-
-    public function setPriority($priority) {
-        $this->priority = $priority;
-    }
-
-    public function build()
+    public function __construct(string $lastmod = '2020-04-21T21:20:48+00:00', string $priority = '1.0')
     {
-        $header='<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml">';
-        $footer="</urlset>";
-        $home='<url>
-                <loc>'.$_ENV["APP_URL"].'</loc>
-                <lastmod>2020-04-21T21:20:48+00:00</lastmod>
+        $this->lastmod = $lastmod;
+        $this->priority = $priority;
+    }
+
+    public function buildFromDB()
+    {
+        $arrPosts = $this->searchFromDatabase();
+        $strPost = '';
+        if ($arrPosts->num_rows > 0) {
+            while ($row = $arrPosts->fetch_assoc()) {
+                if ($row['post_type'] != 'post' && $row['post_type'] != 'page') {
+                    $loc = $row['post_type'].'/'.$row['loc'];
+                } else {
+                    $loc = $row['loc'];
+                }
+
+                $changefreq = $this->formatarData($row['changefreq']);
+                $strPost .= '<url>
+                <loc>'.$_ENV['APP_URL'].'/'.$loc.'</loc>
+                <lastmod>'.$changefreq.'</lastmod>
+                <changefreq>daily</changefreq>
+                <priority>'.$this->priority.'</priority>
+                </url>';
+            }
+        }
+        $this->endFile($strPost);
+
+        return '';
+    }
+
+    public function searchFromDatabase()
+    {
+        $DB_HOST = $_ENV['DB_HOST'];
+        $DB_TABLE = $_ENV['DB_TABLE'];
+        $DB_DATABASE = $_ENV['DB_DATABASE'];
+        $DB_USERNAME = $_ENV['DB_USERNAME'];
+        $DB_PASSWORD = $_ENV['DB_PASSWORD'];
+        $query = "
+        SELECT post_name as 'loc', post_modified as 'changefreq', post_type
+        FROM ".$DB_DATABASE.'.'.$DB_TABLE."
+        WHERE post_type in ('post','testes','frases','textos','page')
+        AND ID not in ('8750','6350')
+        AND post_status = 'publish';";
+        $conn = new \mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+        if ($conn->connect_error) {
+            exit('Falha na conexão: '.$conn->connect_error);
+        }
+
+        $data = $conn->query($query);
+        if ($data) {
+            return $data;
+        } else {
+            return null;
+        }
+    }
+
+    public function endFile($strPost)
+    {
+        $sitemapBuild = $this->getHeader().$this->getHome().$strPost.$this->getFooter();
+        $fp = fopen('mySitemap.xml', 'w');
+        fwrite($fp, $sitemapBuild);
+        fclose($fp);
+
+        return '';
+    }
+
+    public function getHeader()
+    {
+        return '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml">';
+    }
+
+    public function getHome()
+    {
+        return '<url>
+                <loc>'.$_ENV['APP_URL'].'</loc>
+                <lastmod>'.$this->lastmod.'</lastmod>
                 <changefreq>daily</changefreq>
                 <priority>'.$this->priority.'</priority>
             </url>';
+    }
 
-        $arrPosts=$this->searchDatabase();
+    public function getFooter()
+    {
+        return '</urlset>';
+    }
 
-        $strPost="";
-        if ($arrPosts->num_rows > 0) {
-            while ($row = $arrPosts->fetch_assoc()) {
-                if($row["post_type"] != "post" && $row["post_type"] != "page" )
-                    $loc = $row["post_type"]."/". $row["loc"];
-                else
-                    $loc = $row["loc"];
+    public function buildFromFile()
+    {
+        $arrPosts = [];
+        $strPost = '';
+        $handle = $this->fileIsImported();
 
-                $changefreq = $this->formatarData($row["changefreq"]);
-                $strPost.= '<url>
-                <loc>'.$_ENV["APP_URL"].'/'.$loc.'</loc>
-                <lastmod>'. $changefreq.'</lastmod>
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                $strPost .= '<url>
+                <loc>'.trim($line).'</loc>
+                <lastmod>'.$this->lastmod.'</lastmod>
                 <changefreq>daily</changefreq>
                 <priority>'.$this->priority.'</priority>
                 </url>';
             }
         }
 
-        $sitemapBuild = $header.$home.$strPost.$footer;
-        $fp = fopen('sitemap.xml', 'w');
-        $sitemap = fwrite($fp, $sitemapBuild);
-        fclose($fp);
-        return "";
+        $this->endFile($strPost);
+
+        return '';
     }
-   public function searchDatabase()
-   {
-        $DB_HOST=$_ENV["DB_HOST"];
-        $DB_TABLE=$_ENV["DB_TABLE"];
-        $DB_DATABASE=$_ENV["DB_DATABASE"];
-        $DB_USERNAME=$_ENV["DB_USERNAME"];
-        $DB_PASSWORD=$_ENV["DB_PASSWORD"];
-        $query = "
-        SELECT post_name as 'loc', post_modified as 'changefreq', post_type
-        FROM ". $DB_DATABASE .".".$DB_TABLE."
-        WHERE post_type in ('post','testes','frases','textos','page')
-        AND ID not in ('8750','6350')
-        AND post_status = 'publish';";
-        $conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
-        if ($conn->connect_error) {
-            die("Falha na conexão: " . $conn->connect_error);
-        }
 
-        // Executa a consulta
-        $data = $conn->query($query);
-        if($data):
-            return $data;
-        else :
-            return null;
-        endif;
-   }
+    public function fileIsImported()
+    {
+        return fopen('pages.dat', 'r');
+    }
 
-    public function formatarData($dataHora) {
+    public function formatarData($dataHora)
+    {
         // Cria um objeto DateTime a partir da string recebida
-        $data = DateTime::createFromFormat('Y-m-d H:i:s', $dataHora);
+        $data = \DateTime::createFromFormat('Y-m-d H:i:s', $dataHora);
 
         // Verifica se a criação do objeto DateTime foi bem-sucedida
         if ($data === false) {
-            return "Formato de data inválido.";
+            return 'Formato de data inválido.';
         }
 
         // Retorna a data formatada no formato desejado
         return $data->format('Y-m-d\TH:i:sP');
     }
 }
-
-
--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-
-use Symfony\Component\Dotenv\Dotenv;
+require __DIR__.'/vendor/autoload.php';
 
 use App\Class\SiteMap;
-
+use Symfony\Component\Dotenv\Dotenv;
 
 $dotenv = new Dotenv();
 $dotenv->loadEnv(__DIR__.'/.env');
 
-$sitemap = new SiteMap;
-$sitemap->build();
+$sitemap = new SiteMap('2024-06-24T08:00:01+00:00', '0.1');
+/* $sitemap->buildFromDB(); */
 
+$sitemap->buildFromFile();
 
-
-
-
+echo 'Done!';
